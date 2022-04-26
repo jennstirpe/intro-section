b380b85008c7cb3f12cea27b8f6b4dc483dbe29b
jstirpe <jennstirpe@yahoo.com>
2022-04-26 19:37:13 -0400

    Fixed issue with nav display when switching between views

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const companyArrow = document.querySelector("#company-arrow");
 const companyBtn = document.querySelector("#company-btn");
 
 
-
+// show/hide overlay
 openIcon.addEventListener("click", () => {
     nav.classList.remove("main-nav-close");
     overlay.classList.add("display-overlay");
@@ -26,16 +26,33 @@ closeIcon.addEventListener("click", () => {
     overlay.classList.remove("display-overlay");
 })
 
+
 featuresBtn.addEventListener("click", () => {
+    // toggle Features dropdown open&close
     featuresDropdown.classList.toggle("sub-nav-close");
     featuresDropdown.classList.toggle("active");
     featuresArrow.classList.toggle("arrow-up");
-   
+
+    // close Company dropdown if it's open (in desktop view)
+    if(screen.width >= 1440) {
+        companyDropdown.classList.add("sub-nav-close");
+        companyDropdown.classList.remove("active");
+        companyArrow.classList.remove("arrow-up");
+    }
     
 })
 
+
 companyBtn.addEventListener("click", () => {
+    // toggle Company dropdown open&close
     companyDropdown.classList.toggle("sub-nav-close");
     companyDropdown.classList.toggle("active");
     companyArrow.classList.toggle("arrow-up");
+
+    // close Features dropdown if it's open (in desktop view)
+    if(screen.width >= 1440) {
+        featuresDropdown.classList.add("sub-nav-close");
+        featuresDropdown.classList.remove("active");
+        featuresArrow.classList.remove("arrow-up");
+    }
 })
